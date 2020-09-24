d8868366e60d4b7df4ea9ca6a888abb08548fbc0
abattagl <abattagl@ROVERF.fritz.box>
2020-09-24 16:02:34 +0200

    Quarkus bom upgraded to 1.8.0.Final

--- src/main/java/com/redhat/qiot/datahub/query/persistence/MeasurementStationRepository.java
+++ src/main/java/com/redhat/qiot/datahub/query/persistence/MeasurementStationRepository.java
@@ -1,53 +1,53 @@
-package com.redhat.qiot.datahub.query.persistence;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
-
-import org.bson.codecs.configuration.CodecRegistry;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.slf4j.Logger;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.redhat.qiot.datahub.query.domain.station.MeasurementStation;
-import com.redhat.qiot.datahub.query.util.producers.StationCodecRegistryQualifier;
-
-import io.quarkus.runtime.StartupEvent;
-
-@ApplicationScoped
-public class MeasurementStationRepository {
-
-    @ConfigProperty(name = "qiot.database.name")
-    String DATABASE_NAME;
-
-    @ConfigProperty(name = "qiot.measurement.grain.day.collection-name")
-    String COLLECTION_NAME;
-
-    @Inject
-    Logger LOGGER;
-
-    @Inject
-    MongoClient mongoClient;
-
-    MongoDatabase qiotDatabase = null;
-    MongoCollection<MeasurementStation> collection = null;
-    
-    @Inject
-    @StationCodecRegistryQualifier
-    CodecRegistry pojoCodecRegistry;
-
-    void onStart(@Observes StartupEvent ev) {
-    }
-
-    @PostConstruct
-    void init() {
-        qiotDatabase = mongoClient.getDatabase(DATABASE_NAME);
-        collection = qiotDatabase.getCollection(COLLECTION_NAME, MeasurementStation.class);
-
-        collection = collection.withCodecRegistry(pojoCodecRegistry);
-    }
-
-}
+//package com.redhat.qiot.datahub.query.persistence;
+//
+//import javax.annotation.PostConstruct;
+//import javax.enterprise.context.ApplicationScoped;
+//import javax.enterprise.event.Observes;
+//import javax.inject.Inject;
+//
+//import org.bson.codecs.configuration.CodecRegistry;
+//import org.eclipse.microprofile.config.inject.ConfigProperty;
+//import org.slf4j.Logger;
+//
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoDatabase;
+//import com.redhat.qiot.datahub.query.domain.station.MeasurementStation;
+//import com.redhat.qiot.datahub.query.util.producers.StationCodecRegistryQualifier;
+//
+//import io.quarkus.runtime.StartupEvent;
+//
+//@ApplicationScoped
+//public class MeasurementStationRepository {
+//
+//    @ConfigProperty(name = "qiot.database.name")
+//    String DATABASE_NAME;
+//
+//    @ConfigProperty(name = "qiot.measurement.grain.day.collection-name")
+//    String COLLECTION_NAME;
+//
+//    @Inject
+//    Logger LOGGER;
+//
+//    @Inject
+//    MongoClient mongoClient;
+//
+//    MongoDatabase qiotDatabase = null;
+//    MongoCollection<MeasurementStation> collection = null;
+//    
+//    @Inject
+//    @StationCodecRegistryQualifier
+//    CodecRegistry pojoCodecRegistry;
+//
+//    void onStart(@Observes StartupEvent ev) {
+//    }
+//
+//    @PostConstruct
+//    void init() {
+//        qiotDatabase = mongoClient.getDatabase(DATABASE_NAME);
+//        collection = qiotDatabase.getCollection(COLLECTION_NAME, MeasurementStation.class);
+//
+//        collection = collection.withCodecRegistry(pojoCodecRegistry);
+//    }
+//
+//}
